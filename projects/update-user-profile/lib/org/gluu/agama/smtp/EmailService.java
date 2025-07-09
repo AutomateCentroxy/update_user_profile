547aa13f36445bc1e069eb47dfe68fcd55fc0945
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-07-09 17:29:08 +0530

    refactor: update EmailService.java

--- projects/update-user-profile/lib/org/gluu/agama/smtp/EmailService.java
+++ projects/update-user-profile/lib/org/gluu/agama/smtp/EmailService.java
@@ -6,7 +6,7 @@ import org.gluu.agama.smtp.jans.model.ContextData;
 public abstract class EmailService {
 
     public abstract Map<String, String> getUserEntity(String email);
-    public abstract String sendEmail(String to, ContextData context);
+    public abstract String sendEmail(String to);
     public abstract String onboardUser(Map<String, String> profile, Set<String> attributes, String extUid) throws Exception;
 
     public static EmailService getInstance(){
