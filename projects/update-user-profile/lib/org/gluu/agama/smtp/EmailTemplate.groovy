de8e6ed4688b4316f4a10e6e2d7b27bf525bae32
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-08-01 14:54:20 +0530

    refactor: update EmailTemplate.groovy

--- projects/update-user-profile/lib/org/gluu/agama/smtp/EmailTemplate.groovy
+++ projects/update-user-profile/lib/org/gluu/agama/smtp/EmailTemplate.groovy
@@ -8,17 +8,10 @@ class EmailTemplate {
 
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, YYYY, HH:mma (O)");
 
-    /**
-     * Generates an HTML email template for username update notification
-     * @param username New username of the user
-     * @param context Context data containing device, timezone, location info
-     * @return HTML email content
-     */
     static String get(String username, ContextData context) {
-        """
+        return """
 <div style="width: 640px; font-size: 18px; font-family: 'Roboto', sans-serif; font-weight: 300">
-    <div style="background-color: #b6f6da; border-bottom: 1px solid #0ca65d">
-        <img src="https://gluu.org/wp-content/uploads/elementor/thumbs/Logo-qbe8p4qgmufqni0becxda6fnfib6krzb65uihag270.png" alt="Gluu Inc." />
+    <div style="background-color: #b6f6da; border-bottom: 1px solid #0ca65d; height: 60px;">
     </div>
     <div style="padding: 12px; border-bottom: 1px solid #ccc;">
         <p>
@@ -44,19 +37,20 @@ class EmailTemplate {
     <div style="padding: 12px; background-color: #ecf0f5; font-size: 16px">
         <p style="color: #48596b; font-weight: 500">When and where this happened:</p>
         <p><span style="color: #48596b; font-weight: 500">Date:</span><br>${computeDateTime(context.timeZone)}</p>
-        <p><span style="color: #48596b; font-weight: 500">${(context.device == null || context.device.length() == 0) ? '' : ('Device:</span><br>' + context.device)}</p>
-        <p><span style="color: #48596b; font-weight: 500">${(context.location == null || context.location.length() == 0) ? '' : ('Approximate Location:</span><br>' + context.location)}</p>
+        <p><span style="color: #48596b; font-weight: 500">${(context.device == null || context.device.length() == 0) ? "" : ("Device:</span><br>" + context.device)}</p>
+        <p><span style="color: #48596b; font-weight: 500">${(context.location == null || context.location.length() == 0) ? "" : ("Approximate Location:</span><br>" + context.location)}</p>
     </div>
 </div>
-        """
+        """;
     }
 
     private static String computeDateTime(String zone) {
-        Instant now = Instant.now()
+        Instant now = Instant.now();
         try {
-            return now.atZone(ZoneId.of(zone)).format(formatter)
+            return now.atZone(ZoneId.of(zone)).format(formatter);
         } catch (Exception e) {
-            return now.atOffset(ZoneOffset.UTC).format(formatter)
+            return now.atOffset(ZoneOffset.UTC).format(formatter);
         }
     }
 }
+
